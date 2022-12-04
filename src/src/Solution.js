fe30121a0f8ac169cd80072c54500f9ce236c1aa
Thiago Salviato <thiago_salviato@outlook.com>
2022-12-04 10:57:47 -0300

    [Add Some Problems Solutions]

--- src/src/Solution.js
+++ src/src/Solution.js
@@ -113,6 +113,23 @@ function expandFromCenter(s,left,right){
     return s.slice(left - i, right + i + 1);
 };
 
+const convert = (s, numRows) => {
+    if(numRows === 1 || s.length < numRows) return s;
+
+    let direction = false;
+    let count = 0;
+    let arr = new Array(numRows).fill("");
+
+    for (let i = 0; i < s.length; i++) {
+
+        let curr = s[i];
+
+        arr[count] += curr;
+        if(count === 0 || count >= numRows -1) direction = !direction
+        direction ? count ++ : count --;
+    }
+    return arr.join("");
+};
 
 
 
