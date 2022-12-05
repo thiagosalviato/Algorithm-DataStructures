0c2e347cfe508e1ba4cb9e1261942aeab4e919c3
Thiago Salviato <thiago_salviato@outlook.com>
2022-12-05 12:39:52 -0300

    [Add Some Problems Solutions]

--- src/src/Solution.js
+++ src/src/Solution.js
@@ -143,36 +143,42 @@ const reverse = x => {
     return result;
 };
 
+const myAtoi = s => {
+    let result = 0
+    let sign = 1
+    let isNonWhiteSpaceMet = false
+    let isNumberPhase = false
 
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i]
+        if (char === ' ') {
+            if (!isNonWhiteSpaceMet) {
+                continue
+            } else {
+                break
+            }
+        }
+        isNonWhiteSpaceMet = true
 
+        if (char >= '0' && char <= '9') {
+            isNumberPhase = true
+            result = result * 10 + (char - '0')
+            continue
+        }
 
+        if (char === '+' && !isNumberPhase) {
+            isNumberPhase = true
+            continue
+        }
 
+        if (char === '-' && !isNumberPhase) {
+            isNumberPhase = true
+            sign *= -1
+            continue
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        break
+    }
+    result *= sign
+    return Math.min(Math.max(-(2 ** 31), result), 2**31 - 1)
+};
