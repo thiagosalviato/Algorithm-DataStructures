a497339b731ec5601be79f5ecb357a3831cddf49
Thiago Salviato <thiago_salviato@outlook.com>
2023-01-04 20:48:40 -0300

    [Add Some Problems Solutions]

--- src/Solution.java
+++ src/Solution.java
@@ -131,4 +131,18 @@ public class Solution {
         }
         return maxAreaBetween;
     }
+
+    public String intToRoman(int num) {
+        int[] values = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String[] roman = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        StringBuilder romanVersion = new StringBuilder();
+
+        for (int i = values.length;i>=0 && num > 0;i--){
+            while(num >= values[i]){
+                num -= values[i];
+                romanVersion.append(roman[i]);
+            }
+        }
+        return romanVersion.toString();
+    }
 }
--- src/Solution.js
+++ src/Solution.js
@@ -273,3 +273,13 @@ const maxArea = (height) => {
     return maxAreaBetween;
 }
 
+const intToRoman = (num) =>{
+    let values = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    let roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    let romanVersion = "";
+
+    for (let i = 0; num; i++)
+        while (num >= values[i]) romanVersion += roman[i], num -= values[i]
+    return romanVersion;
+    
+};
