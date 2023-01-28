818cd76d29b625bc47ac6c0cb2e33e15cee0fafa
thiagosalviato <thiago_salviato@outlook.com>
2023-01-28 08:59:46 -0300

    [Add Some Problems Solutions]

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Solution {
 
     int resultStart;
@@ -290,5 +292,20 @@ public class Solution {
         slow.next = slow.next.next;
 
         return list.next;
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
     }
 }
